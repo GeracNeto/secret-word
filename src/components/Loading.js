5761223497e9df6482c3acc605c1418e7d3a8ca5
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-08-16 16:19:28 -0300

    Criado componente Loading
    Componente de animação para carregamento da página ao clicar em "começar jogo"

---
+++ src/components/Loading.js
@@ -0,0 +1,12 @@
+import './Loading.css'
+
+const Loading = () => {
+    return (
+        <>
+            <div className='loading'></div>
+            <p id='loading-p'>Gerando palavras...</p>
+        </>
+    )
+}
+
+export default Loading
