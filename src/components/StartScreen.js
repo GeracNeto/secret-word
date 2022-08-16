a0385f2898c962126242382a83aba4a21b3e56dd
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-08-16 16:21:03 -0300

    Atualizado componente StartScreen.js
    Criada função que executa a animação de loading na página e executa a função prop startGame

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,11 +1,25 @@
+import { useState } from 'react';
 import './StartScreen.css';
 
+// Components
+import Loading from './Loading';
+
 const StartScreen = ({ startGame }) => {
+
+    const [loadGame, setLoadGame] = useState(false)
+
+    const handleStartGame = () => {
+        setLoadGame(true)
+
+        startGame()
+    }
+
     return (
         <div className='start'>
             <h1>Secret Word</h1>
             <p>Clique no botão abaixo para começar a jogar</p>
-            <button onClick={startGame}>Começar Jogo</button>
+            <button onClick={handleStartGame}>Começar Jogo</button>
+            {loadGame && <Loading />}
         </div>
     )
 }
