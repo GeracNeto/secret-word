3b5cec453a1a49dfd941dd7f3008e28fa52f526b
Geraldo Pereira da Costa Neto <80066270+GeracNeto@users.noreply.github.com>
2022-08-17 10:09:23 -0300

    Criado footer da página

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+// Styles
+import './Footer.css'
+
+const Footer = () => {
+
+    return(
+        <footer>
+            <p>Powered by: Geraldo Pereira da Costa Neto</p>
+            <a href="https://www.linkedin.com/in/geraldo-costa-5568a8152/" target='_blank' rel="noreferrer"><img src="./linkedin.png" alt="Linkedin-img" /></a>
+            <a href="https://github.com/GeracNeto" target='_blank' rel="noreferrer"><img src="./github.png" alt="Github-img" /></a>
+        </footer>
+    )
+}
+
+export default Footer
